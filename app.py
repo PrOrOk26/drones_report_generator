8eedf30b92016bafb11ca1e2c58ef72d412ff8c3
Vlad Raniuk <vladgold2699@gmail.com>
2023-05-13 16:11:27 +0300

    revert the old app.py file

---
+++ app.py
@@ -0,0 +1,18 @@
+# Add this to app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/hw')
+def hello_world2():
+    return 'Hello World 2!'
+
+
+if __name__ == "__main__":
+    app.run()
